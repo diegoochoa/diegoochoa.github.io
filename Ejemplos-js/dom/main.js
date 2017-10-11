d22a0f978a65af5d017d7cf3f0653c52d3d2eb93
Diego <dialogo.crg@gmail.com>
2017-10-11 12:00:17 -0500

    Boton JS

--- Ejemplos-js/dom/main.js
+++ Ejemplos-js/dom/main.js
@@ -1,6 +1,35 @@
-var nuevo = document.createElement('div');
-var texto = document.createTextNode('Caja4');
-nuevo.appendChild(texto);
-nuevo.setAttribute("class","caja");
-var padre = document.getElementsByTagName('body');
-padre[0].appendChild(nuevo);
+var btnAgregar = document.getElementById('btnAgregar');
+/*Event handler */
+//btnAgregar.onclick=agregaCaja;
+//btnAgregar.onclick=function(){
+    //window.alert('Ejecutando funcion anonima');
+//};
+
+/*event listener */
+btnAgregar.addEventListener('click',function(){
+    var entrada = document.getElementById('entrada');
+    var contenido = entrada.value;
+    if(contenido!='')
+    {
+        agregaCaja(contenido);
+    }
+    
+});
+//document.addEventListener('click',agregaCaja);
+// document.addEventListener('click',prueba);
+// document.addEventListener('click',function(){window.alert('Probando funcion anonima')});
+
+function agregaCaja(texto)
+{
+    var nuevo = document.createElement('div');
+    var texto = document.createTextNode(texto);
+    nuevo.appendChild(texto);
+    nuevo.setAttribute("class","caja");
+    var padre = document.getElementsByTagName('body');
+    padre[0].appendChild(nuevo);
+}
+
+function prueba()
+{
+    window.alert('Hola');
+}
